1cb61b7c6eb2860a3984b2ccc9fb944677f8cd76
Walid Jamarin <walidjamarin21@gmail.com>
2024-10-02 23:24:45 +1000

    fix typo error in backend

--- job-board-server/index.js
+++ job-board-server/index.js
@@ -7,17 +7,18 @@ const app = express();
 app.use(cors());
 
 
-const PORT = env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // ENDPOINT for SERPAPI
 app.get('/api/jobs', async (req, res ) => {
     const { query } = req.query;
 
-    try {
-        const serpAPIURL = `https://serpapi.com/search.json?engine=google_jobs&q=${query}&api_key${process.env.SERP_API_KEY}`;
-        const  response = await axios.get(serpAPIURL);
-        console.log(JSON.stringify(response));
-        res.json(response.data.job_results || []);
+    try {   
+        const serpAPIURL = `https://serpapi.com/search.json?engine=google_jobs&q=${query}&api_key=${process.env.SERP_API_KEY}`;
+        console.log('serpAPIURL', serpAPIURL);
+        const response = await axios.get(serpAPIURL);
+        console.log('response:', JSON.stringify((response.data.jobs_results)));
+        res.json(response.data.jobs_results || []);
     } catch (error) {
       res.status(500).json({ error: error.message})
     }
